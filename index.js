cf43309655849bc4b44c6d402cfc1f8d4360a0a9
George Irwin <jedimastag0@gmail.com>
2023-03-26 17:08:22 -0400

    complete funtions lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks <= 42) {
+        return 42 - blocks;
+    } else return blocks - 42
+}
+
+function distanceFromHqInFeet(blocks) {
+    return (distanceFromHqInBlocks(blocks) * 264);
+
+}
+
+function distanceTravelledInFeet(start, end) {
+    return (Math.abs(start - end) * 264);
+}
+
+function calculatesFarePrice(start, end){
+    let fareDistance = distanceTravelledInFeet(start, end);
+
+    if (fareDistance <= 400){
+        return 0;
+    } 
+    else if (fareDistance >= 400 && fareDistance <= 2000) {
+        return (fareDistance -400) * .02;
+    }
+    else if (fareDistance > 2000 && fareDistance <= 2500) {
+        return 25;
+    }
+    else {
+        return "cannot travel that far"
+    }
+}
